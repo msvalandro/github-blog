37be9b11261a155bb959af9ef22ef381ddcc0ee2
msvalandro <msvalandro@gmail.com>
2024-06-07 10:38:14 +0100

    create initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {
+      blue: '#3294F8',
+
+      'base-title': '#E7EDF4',
+      'base-subtitle': '#C4D4E3',
+      'base-text': '#AFC2D4',
+      'base-span': '#7B96B2',
+      'base-label': '#3A536B',
+      'base-border': '#1C2F41',
+      'base-post': '#112131',
+      'base-profile': '#0B1B2B',
+      'base-background': '#071422',
+      'base-input': '#040F1A',
+    },
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      m: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
