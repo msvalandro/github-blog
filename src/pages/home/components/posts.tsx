c54887b76542250759cd651048a77f23e3608966
msvalandro <msvalandro@gmail.com>
2024-06-07 19:42:25 +0100

    feat: fetch issues from github repo

--- src/pages/home/components/posts.tsx
+++ src/pages/home/components/posts.tsx
@@ -1,11 +1,45 @@
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
+import { api } from '../../../lib/axios'
+
+const REPOSITORY_URL = 'msvalandro/github-blog'
+
+interface Post {
+  id: number
+  title: string
+  body: string
+  created_at: string
+}
+
 export function Posts() {
+  const [posts, setPosts] = useState<Post[]>([])
+
+  async function fetchPosts() {
+    try {
+      const response = await api.get('/search/issues', {
+        params: {
+          q: `${''}repo:${REPOSITORY_URL}`,
+        },
+      })
+
+      setPosts(response.data.items)
+    } catch {}
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return (
     <>
       <div className="flex justify-between">
         <h2 className="text-lg font-bold text-base-subtitle">Publicações</h2>
-        <span className="text-s text-base-span">6 publicações</span>
+        <span className="text-s text-base-span">
+          {posts.length} publicações
+        </span>
       </div>
 
       <input
@@ -15,31 +49,25 @@ export function Posts() {
       />
 
       <ul className="mt-12 grid grid-cols-2 gap-8">
-        {[1, 2, 3, 4, 5, 6].map((_, index) => (
+        {posts.map((post) => (
           <li
-            key={index}
+            key={post.id}
             className="h-[260px] overflow-hidden rounded-[10px] bg-base-post p-8"
           >
             <Link to="post/1">
               <header className="flex justify-between">
-                <h1 className="text-xl font-bold text-base-title">
-                  JavaScript data types and data structures
+                <h1 className="title-ellipsis text-xl font-bold text-base-title">
+                  {post.title}
                 </h1>
-                <span className="ml-4 w-[80px] text-end text-sm text-base-span">
-                  Há 1 dia
+                <span className="ml-4 w-[100px] text-end text-sm text-base-span">
+                  {formatDistanceToNow(new Date(post.created_at), {
+                    locale: ptBR,
+                    addSuffix: true,
+                  })}
                 </span>
               </header>
 
-              <p className="paragraph-ellipsis mt-4">
-                Programming languages all have built-in data structures, but
-                these often differ from one language to another. This article
-                attempts to list the built-in data structures available in
-                JavaScript and what properties they have. These can be used to
-                build other data structures. Wherever possible, comparisons with
-                other languages are drawn. Dynamic typing JavaScript is a
-                loosely typed and dynamic language. Variables in JavaScript are
-                not directly
-              </p>
+              <p className="body-ellipsis mt-4">{post.body}</p>
             </Link>
           </li>
         ))}
